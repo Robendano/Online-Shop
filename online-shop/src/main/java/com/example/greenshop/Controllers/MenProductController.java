ffbd716161f8dfe8c01a4e05f7b479d8ac575252
Robendano <78977590+Robendano@users.noreply.github.com>
2022-06-02 12:09:44 +0600

    Swagger added

--- online-shop/src/main/java/com/example/greenshop/Controllers/MenProductController.java
+++ online-shop/src/main/java/com/example/greenshop/Controllers/MenProductController.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@RestController
 public class MenProductController {
 
     @Autowired
