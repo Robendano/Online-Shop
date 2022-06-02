d9bbf8ef505b8e45ea31d53116f7901830e9d882
Robendano <78977590+Robendano@users.noreply.github.com>
2022-06-02 12:07:20 +0600

    Swagger added

--- online-shop/src/main/java/com/example/greenshop/Controllers/AddProduct.java
+++ online-shop/src/main/java/com/example/greenshop/Controllers/AddProduct.java
@@ -9,11 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 import java.util.ArrayList;
 import java.util.Optional;
 
-@Controller
+@EnableSwagger2
+@RestController
 public class AddProduct {
 
     @Autowired
